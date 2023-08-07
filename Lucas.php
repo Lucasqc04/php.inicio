94ed3198356cab5f9d98188b1b934f04d8784a9a
lucasqc04 <lucasqcampos9@gmail.com>
2023-08-07 20:28:56 -0300

    teste php

---
+++ Lucas.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+<?php
+
+$nome = "lucas";
+$adjetivo = "Legal";
+$linguagem = "PHP";
+$tempo_min = 5;
+
+echo $nome . " é muito " . $adjetivo . ", e fez uma aplicação em " . $linguagem . " em " . $tempo_min . " minutos."
+
+?>
+
+</html>
+</body>
